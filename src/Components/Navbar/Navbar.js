de409e4b217874052ff165cd87c3a61b15a39529
YannYiss <adrian_nc@outlook.com>
2022-01-17 20:06:29 -0600

    Views created and Know me finished

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
+import logo_w from '../../Assets/nico_logo_w.png'
 
 export default function Navbar() {
     return (
         <nav className='navbar'>
+            <Link to='/'><img src={logo_w} alt='NiCo' id='logo_w'/></Link>
             <ul className='links'>
                 <Link to='/know-me'><li>Know Me</li></Link>
                 <Link to='/projects'><li>Projects</li></Link>
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -8,8 +8,8 @@ export default function Sidebar() {
         <aside className='sidebar'>
             <Link to='/'><img src={logo} alt='NiCo' id='logo'/></Link>
             <div className='link-icons'>
-                <i className="fab fa-linkedin"></i>
-                <i className="fab fa-github"></i>
+                <a href='https://www.linkedin.com/in/adriannietocovarrubias/'><i className="fab fa-linkedin"></i></a>
+                <a href='https://github.com/YannYiss'><i className="fab fa-github"></i></a>
             </div>
         </aside>
     )
