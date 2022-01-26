2e37e4471f1f038cf493b9adf33bf757211707bf
YannYiss <adrian_nc@outlook.com>
2022-01-25 21:32:08 -0600

    Contac view in progress

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Contact.css';
+import Sidebar from '../../Components/Sidebar';
+import {Form, Button} from 'react-bootstrap';
+import '@animxyz/core';
+import {XyzTransitionGroup} from '@animxyz/react';
 
 export default function Contact() {
     return (
         <div>
-            
+            <Sidebar/>
+            <main className='contact_main'>
+                <div className='contact_description'>
+                    <h2>Let's get in</h2>
+                    <h2 id="touch">Touch!</h2>
+                </div>
+                <Form className='form'transition-style='in:polygon:opposing-corners'>
+                    <Form.Group className="mb-3" controlId="name_input">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="last_name">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your last name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="comment">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder="Type in your quesiton or comment" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </main>
         </div>
     )
 }
--- src/Views/Know-me/Know_me.js
+++ src/Views/Know-me/Know_me.js
@@ -7,6 +7,8 @@ import leon from '../../Assets/leon.jpg';
 import study from '../../Assets/studyjpg.jpg';
 import code from '../../Assets/pexels-kevin-ku-577585.jpg';
 import blueprint from '../../Assets/blueprint.jpg';
+import '@animxyz/core';
+import {XyzTransitionGroup} from '@animxyz/react';
 
 export default function Know_me() {
     return (
@@ -22,8 +24,10 @@ export default function Know_me() {
             <div className='bio'>
                 <div className='bio_section'>
                     <div className='p'>
-                        <p>I'm from Mexico City but I had lived at Leon, Guanajuato, the central part of México called Bajio, since I was 8.</p>
-                        <p>I'm 31, married and dad of a 2 years old wonderful girl named Camila. </p>
+                        <XyzTransitionGroup appearVisible xyz='fade duration-10'>
+                            <p>I'm from Mexico City but I had lived at Leon, Guanajuato, the central part of México called Bajio, since I was 8.</p>
+                            <p>I'm 31, married and dad of a 2 years old wonderful girl named Camila. </p>
+                        </XyzTransitionGroup>
                     </div>
                     <div className='p_image'>
                         <img id='mex' src={mex} alt='Mexico City'/>
@@ -32,26 +36,30 @@ export default function Know_me() {
                 </div>
                 <div className='bio_section'>
                     <div className='p_image_lg'>
-                        <img src={blueprint} className='bio_image_lg_lft' alt='Mexico City'/>
+                            <img src={blueprint} className='bio_image_lg_lft' alt='Mexico City'/>
                     </div>
                     <div className='p'>
-                        <p>I studied Industrial Engeneering, but my true love is for techology, reazon why I decided to get into coding.</p>
-                        <p>I always liked to learn stuff by myself so I started on mw own in my coding path. Currently, I'm taking the Master in Coding course provided by Dev.F, which will boost my skills to become a full-stack developer.</p>
+                        <XyzTransitionGroup appearVisible xyz='fade duration-10'>
+                            <p>I studied Industrial Engeneering, but my true love is for techology, reazon why I decided to get into coding.</p>
+                            <p>I always liked to learn stuff by myself so I started on mw own in my coding path. Currently, I'm taking the Master in Coding course provided by Dev.F, which will boost my skills to become a full-stack developer.</p>
+                        </XyzTransitionGroup>
                     </div>
                 </div>
                 <div className='bio_section'>
                     <div className='p'>
-                        <p>Some of the front-end technologies that I currently handle includes:</p>
-                        <ul>
-                            <li>HTML 5</li>
-                            <li>CSS 3</li>
-                            <li>Bootstrap</li>
-                            <li>SASS</li>
-                            <li>JavaScript ES6</li>
-                            <li>React</li>
-                            <li>Node.js</li>
-                        </ul>
-                        <p>Just to mention some...</p>
+                        <XyzTransitionGroup appearVisible xyz='fade duration-10'>
+                            <p>Some of the front-end technologies that I currently handle includes:</p>
+                            <ul>
+                                <li>HTML 5</li>
+                                <li>CSS 3</li>
+                                <li>Bootstrap</li>
+                                <li>SASS</li>
+                                <li>JavaScript ES6</li>
+                                <li>React</li>
+                                <li>Node.js</li>
+                            </ul>
+                            <p>Just to mention some...</p>
+                        </XyzTransitionGroup>
                     </div>
                     <div className='p_image_lg'>
                         <img src={code} className='bio_image_lg_rgt' alt='Coding'/>
@@ -62,15 +70,17 @@ export default function Know_me() {
                         <img src={study} className='bio_image_lg_lft' alt='Studying Hard'/>
                     </div>
                     <div className='p'>
-                        <p>I'll continue learning some technologies in the next months, focused in the back-end development, including:</p>
-                        <ul>
-                            <li>MongoDB</li>
-                            <li>Express.js</li>
-                            <li>RESTful APIs</li>
-                            <li>SQL and NoSQL Databases</li>
-                            <li>Docker</li>
-                            <li>CI/CD</li>
-                        </ul>
+                        <XyzTransitionGroup appearVisible xyz='fade duration-10'>
+                            <p>I'll continue learning some technologies in the next months, focused in the back-end development, including:</p>
+                            <ul>
+                                <li>MongoDB</li>
+                                <li>Express.js</li>
+                                <li>RESTful APIs</li>
+                                <li>SQL and NoSQL Databases</li>
+                                <li>Docker</li>
+                                <li>CI/CD</li>
+                            </ul>
+                        </XyzTransitionGroup>
                     </div>
                 </div>
             </div>
