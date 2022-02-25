1613c0e03acffaa590c6cf54b1c7a4813f061649
YannYiss <adrian_nc@outlook.com>
2022-02-24 22:18:15 -0600

    code splitting, lazy loading and Loading component added to project

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from '../Components/Navbar';
-import Hero from '../Views/Hero';
-import Know_me from '../Views/Know-me/';
-import Projects from '../Views/Projects/Projects';
-import Contact from '../Views/Contact/Contact';
 import ScrollToTop from '../Components/ScrollToTop/ScrollToTop';
+import Loading from '../Views/Loading';
+
+const Hero = lazy(() => import('../Views/Hero'));
+const Know_me = lazy(() => import('../Views/Know-me'));
+const Projects = lazy(() => import('../Views/Projects'));
+const Contact = lazy(() => import('../Views/Contact'));
 
 export default function Routes() {
     return (
         <Router>
             <Navbar/>
             <ScrollToTop/>
-            <Switch>
-                <Route exact path='/' component={Hero}/>
-                <Route exact path ='/know-me' component={Know_me}/>
-                <Route exact path='/projects' component={Projects}/>
-                <Route exact path='/contact' component={Contact}/>
-            </Switch>
+            <Suspense fallback={<Loading/>}>
+                <Switch>
+                    <Route exact path='/' component={Hero}/>
+                    <Route exact path ='/know-me' component={Know_me}/>
+                    <Route exact path='/projects' component={Projects}/>
+                    <Route exact path='/contact' component={Contact}/>
+                </Switch>
+            </Suspense>
         </Router>
     )
 }
---
+++ src/Views/Loading/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Loading.css';
+
+export default function Loading() {
+  return (
+    <main className='loading_main'>
+        <div className='loading_div'>
+            <div className='loading_frame'>
+                <div className='loading_text'>
+                    <h1>LOADING</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
